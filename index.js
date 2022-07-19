506ee445ac90418e72e54a4b54051ab164af646a
Fiseha <famarkfish79@gmail.com>
2022-07-19 22:59:32 +0300

    Severak Changes to Js and Html

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+setTimeout(viewGrid ,1000);
 function openForm() {
     document.getElementById("mainForm").style.display = "block";
   }
@@ -6,12 +7,12 @@ function openForm() {
     document.getElementById("mainForm").style.display = "none";
   }
 
+var myLibrary = [];
+let book0= { "index":0,"title": 'First Book',"author": 'First Author',"pages":10,"read":"True",}
+myLibrary.push(book0);
 
 
 
-
-
-let myLibrary = [];
 function Book(title, author, pages ,read){
     this.title = title;
     this.author= author;
@@ -21,13 +22,19 @@ function Book(title, author, pages ,read){
     }
 
 function addBookToLibrary(){
-    let title = document.getElementById("myForm").elements.namedItem("Title").value;
-    let author = document.getElementById("myForm").elements.namedItem("Author").value;
-    let pages = document.getElementById("myForm").elements.namedItem("Page-Num").value;
-    let read = document.getElementById("myForm").elements.namedItem("read").value;
 
-    var Book1 = new Book(title, author, pages , read);
-    myLibrary= myLibrary.push(Book1);
+    let books = {
+    // index: 0,
+     title : document.getElementById("myForm").elements.namedItem("Title").value,
+     author : document.getElementById("myForm").elements.namedItem("Author").value,
+     pages : document.getElementById("myForm").elements.namedItem("Page-Num").value,
+     read : document.getElementById("myForm").elements.namedItem("read").value
+    }
+    
+    myLibrary.push(books);
+    document.forms[0].reset();
+
+    localStorage.setItem('MyLibrary', JSON.stringify(myLibrary));
 
 }
 function readOrNot (read){
@@ -42,6 +49,7 @@ function readOrNot (read){
 
 } 
 function viewGrid(){
+  /*
     let title1 = document.getElementById("myForm").elements.namedItem("Title").value;
     let author1 = document.getElementById("myForm").elements.namedItem("Author").value;
     let pages1 = document.getElementById("myForm").elements.namedItem("Page-Num").value;
@@ -52,24 +60,29 @@ function viewGrid(){
      else{
       read1='Not Read'
      }
+*/
+    myLibrary=JSON.parse(window.localStorage.getItem('MyLibrary'));
 
     //read1 = readOrNot(read1);
-
+   for( let i=0; i< myLibrary.length; i++){
     let container = document.querySelector('.main-body');
     let content = document.createElement('div');
      content.classList.add('book-card');
+     //let index = document.createElement('div');
+     //index.textcontent = 'Index:' + myLibrary[i].index;
      let title = document.createElement('div');
-     title.textContent = 'Title: ' + title1;
+     title.textContent = 'Title: ' + myLibrary[i].title;
      let author = document.createElement('div');
-     author.textContent = 'Author: ' + author1;
+     author.textContent = 'Author: ' + myLibrary[i].author;
      let pages = document.createElement('div');
-     pages.textContent = 'Page-Num: ' + pages1;
+     pages.textContent = 'Page-Num: ' + myLibrary[i].pages;
      let read = document.createElement('div');
      
-     read.textContent = 'Read: ' + read1;
+     read.textContent = 'Read: ' + myLibrary[i].read;
      let button = document.createElement('div');
      button.textContent = 'Button: ';
 
+     content.appendChild(index);
      content.appendChild(title);
      content.appendChild(author);
      content.appendChild(pages);
@@ -78,6 +91,6 @@ function viewGrid(){
 
      container.appendChild(content);
      
-
+   }
 
 }
