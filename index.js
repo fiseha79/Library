dcddff3e4c6d3b33edf047e303f0f3bb41d0c636
Fiseha <famarkfish79@gmail.com>
2022-07-27 00:58:37 +0300

    Updates to html,css and javascript,95% done

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-setTimeout(viewGrid ,1000);
+
+
+//DOM Elements
+$container = document.querySelector('.main-body');
+$form=document.getElementById("myForm");
+$titleIn = $form.elements.namedItem("Title");
+$authorIn = $form.elements.namedItem("Author");
+$pagesIn = $form.elements.namedItem("Page-Num");
+$readIn= $form.elements.namedItem("read");
+
+
+
 function openForm() {
     document.getElementById("mainForm").style.display = "block";
   }
@@ -7,90 +18,127 @@ function openForm() {
     document.getElementById("mainForm").style.display = "none";
   }
 
-var myLibrary = [];
-let book0= { "index":0,"title": 'First Book',"author": 'First Author',"pages":10,"read":"True",}
-myLibrary.push(book0);
+var myLibrary = [
+  {
+    title:"First Book",
+    author:"First Author",
+    pages:10,
+    read: 'Read',
+  }
+];
 
-
-
-function Book(title, author, pages ,read){
-    this.title = title;
-    this.author= author;
-    this.pages = pages;
-    this.read = read;
-      
-    }
-
-function addBookToLibrary(){
-
-    let books = {
-    // index: 0,
-     title : document.getElementById("myForm").elements.namedItem("Title").value,
-     author : document.getElementById("myForm").elements.namedItem("Author").value,
-     pages : document.getElementById("myForm").elements.namedItem("Page-Num").value,
-     read : document.getElementById("myForm").elements.namedItem("read").value
-    }
-    
-    myLibrary.push(books);
-    document.forms[0].reset();
-
-    localStorage.setItem('MyLibrary', JSON.stringify(myLibrary));
-
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
-function readOrNot (read){
-  if(read == 'True'){
-    read='read';
+
+const setStorage = () => {
+  localStorage.setItem('library', JSON.stringify(myLibrary));
+}
+
+const getStorage = () => {
+  myLibrary = JSON.parse(localStorage.getItem('library'));
+}
+
+
+const addBookToLibrary = () => {
+  let title = $titleIn.value;
+  let author = $authorIn.value;
+  let pages = $pagesIn.value;
+  let read = readOrNot();
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  document.forms[0].reset();
+  updateGrid();
+}
+
+
+function readOrNot(){
+  
+  if($readIn.checked == true){
+    read1='Read';
    }
    else{
-    read='Not read'
+    read1='Not Read'
    }
 
-   return read;
+   return read1;
 
 } 
-function viewGrid(){
-  /*
-    let title1 = document.getElementById("myForm").elements.namedItem("Title").value;
-    let author1 = document.getElementById("myForm").elements.namedItem("Author").value;
-    let pages1 = document.getElementById("myForm").elements.namedItem("Page-Num").value;
-    let read1 = document.querySelector('#read');
-    if(read1.checked == true){
-      read1='Read';
-     }
-     else{
-      read1='Not Read'
-     }
-*/
-    myLibrary=JSON.parse(window.localStorage.getItem('MyLibrary'));
 
-    //read1 = readOrNot(read1);
-   for( let i=0; i< myLibrary.length; i++){
-    let container = document.querySelector('.main-body');
+const createDeleteDiv = (index) => {
+  let $deleteDiv = document.createElement('div');
+  let $deleteButton = document.createElement('button');
+  $deleteButton.textContent = 'Delete';
+  $deleteButton.addEventListener('click', () => {
+    myLibrary.splice(index, 1);
+    updateGrid();
+  });
+  $deleteDiv.appendChild($deleteButton);
+  return $deleteDiv;
+}
+
+const toggleReadDiv = (book) => {
+  let $toggleReadDiv = document.createElement('div');
+  let $toggleButton = document.createElement('button');
+  $toggleButton.textContent = 'Read/Not Read';
+  $toggleButton.addEventListener('click', () => {
+    book.read = !book.read;
+    
+    updateGrid();
+  });
+  $toggleReadDiv.appendChild($toggleButton);
+  return $toggleReadDiv;
+}
+
+
+
+const updateGrid=()=> {
+  
+   $container.textContent = '';
+
+  myLibrary.forEach((book, index) => {
+
+
     let content = document.createElement('div');
      content.classList.add('book-card');
-     //let index = document.createElement('div');
-     //index.textcontent = 'Index:' + myLibrary[i].index;
+     
      let title = document.createElement('div');
-     title.textContent = 'Title: ' + myLibrary[i].title;
+     title.textContent = 'Title: ' + book.title;
      let author = document.createElement('div');
-     author.textContent = 'Author: ' + myLibrary[i].author;
+     author.textContent = 'Author: ' + book.author;
      let pages = document.createElement('div');
-     pages.textContent = 'Page-Num: ' + myLibrary[i].pages;
+     pages.textContent = 'Page-Num: ' + book.pages;
      let read = document.createElement('div');
      
-     read.textContent = 'Read: ' + myLibrary[i].read;
-     let button = document.createElement('div');
-     button.textContent = 'Button: ';
-
-     content.appendChild(index);
+     read.textContent = 'Read: ' + book.read;
+     
+     
      content.appendChild(title);
      content.appendChild(author);
      content.appendChild(pages);
      content.appendChild(read);
-     content.appendChild(button);
+     content.appendChild(createDeleteDiv(index));
+     content.appendChild(toggleReadDiv(book));
 
-     container.appendChild(content);
+
+     $container.appendChild(content);
      
-   }
+   });
+
+   setStorage();
 
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+ 
+  if(!localStorage.getItem('library')) {
+    setStorage();
+  } else {
+    getStorage();
+  }
+
+  updateGrid();
+});
